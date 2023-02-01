5b1d0bd13af06f43dbb11658f49d3a6baf709793
Emily Nixon <123026549+enixon01@users.noreply.github.com>
2023-02-01 16:04:47 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,9 @@
+#Emily Nixon
+import math
+a = math.tan(math.pi/4)
+n = math.sqrt(a+1)
+b = math.tan(math.pi/3)
+c = math.sin(math.pi/3)
+den= math.sqrt(b +c)
+value = n/den
+print (value)
---
+++ L4.py
@@ -0,0 +1,8 @@
+#Emily Nixon
+import math
+r = 4
+circl = math.pi*r**2
+a = 4*math.sqrt(2)
+sqr = a * a
+value = circl - sqr
+print (value)
